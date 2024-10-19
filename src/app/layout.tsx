113df4fd36f16855780855ff925d5831c3ef3003
Irsyad Abdul <abdulirsyad15@gmail.com>
2024-10-19 18:23:03 +0700

    feat: add quote sign

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Provider } from '@/components/provider/provider';
-import { poppins } from '@/lib/font';
+import { figtree } from '@/lib/font';
 
 export const metadata: Metadata = {
   title: 'Yuk Ngobrol',
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.className}`}>
+      <body className={`${figtree.className}`}>
         <Provider>{children}</Provider>
       </body>
     </html>
--- src/components/question-card/question-card.tsx
+++ src/components/question-card/question-card.tsx
@@ -6,7 +6,25 @@ import { IQuestionCard } from '@/lib/question.interface';
 const QuestionCard = (question: IQuestionCard) => {
   return (
     <div className={styles['question-card']}>
-      <div className={styles.question}>{question.question}</div>
+      <div className={styles.title} style={{ color: question.category.color }}>
+        Yuk Ngobrol
+      </div>
+      <div
+        className={styles.question}
+        style={{ borderColor: `${question.category.color}55` }}
+      >
+        {['top-left', 'bottom-right'].map((position, index) => (
+          <div key={index} className={`${styles.quote} ${styles[position]}`}>
+            <Image
+              src="/images/quote.png"
+              alt="quote sign"
+              width={100}
+              height={100}
+            />
+          </div>
+        ))}
+        <span>{question.question}</span>
+      </div>
       <div
         className={styles.category}
         style={{ backgroundColor: question.category.color }}
