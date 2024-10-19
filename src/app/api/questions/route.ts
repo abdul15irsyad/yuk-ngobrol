03610690f03ae0ce9066360e19ad2e2a245d7e47
Irsyad Abdul <abdulirsyad15@gmail.com>
2024-10-19 21:17:26 +0700

    feat: set try catch on api

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -3,49 +3,57 @@ import { shuffle } from '@/utils/array.util';
 import { NextResponse } from 'next/server';
 
 export const GET = async (request: Request) => {
-  const { searchParams } = new URL(request.url);
-  const search = searchParams.get('search') ?? undefined;
-  const categoryNames = searchParams
-    .get('categoryNames')
-    ?.split(',')
-    ?.map((categoryName) => categoryName.trim());
+  try {
+    const { searchParams } = new URL(request.url);
+    const search = searchParams.get('search') ?? undefined;
+    const categoryNames = searchParams
+      .get('categoryNames')
+      ?.split(',')
+      ?.map((categoryName) => categoryName.trim());
 
-  const where = {
-    category: categoryNames
-      ? {
-          name: {
-            in: categoryNames,
-          },
-        }
-      : undefined,
-    question: search
-      ? {
-          contains: search,
-        }
-      : undefined,
-  };
-  const questions = await prisma.question.findMany({
-    where,
-    select: {
-      question: true,
-      category: {
-        select: {
-          name: true,
-          color: true,
-          logo: {
-            select: {
-              path: true,
-              filename: true,
-              mime: true,
+    const where = {
+      category: categoryNames
+        ? {
+            name: {
+              in: categoryNames,
+            },
+          }
+        : undefined,
+      question: search
+        ? {
+            contains: search,
+          }
+        : undefined,
+    };
+    const questions = await prisma.question.findMany({
+      where,
+      select: {
+        question: true,
+        category: {
+          select: {
+            name: true,
+            color: true,
+            logo: {
+              select: {
+                path: true,
+                filename: true,
+                mime: true,
+              },
             },
           },
         },
       },
-    },
-  });
+    });
 
-  return NextResponse.json({
-    message: 'get questions',
-    data: shuffle(questions),
-  });
+    return NextResponse.json({
+      message: 'get questions',
+      data: shuffle(questions),
+    });
+  } catch (error: unknown) {
+    console.error(error);
+    return NextResponse.json({
+      message: 'internal server error',
+      error,
+    });
+  }
 };
